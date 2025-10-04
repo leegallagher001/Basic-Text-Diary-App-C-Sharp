2c4320d878c8b0e2a8c6a2de7d47884f3181e0d5
leegallagher001 <lee.gallagher.writing@outlook.com>
2025-10-04 17:22:07 +0100

    Add files via upload

---
+++ Basic Text Diary App/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Basic_Text_Diary_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelPageTitle = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelEntry = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.textBoxEntry = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelPageTitle
+            // 
+            this.labelPageTitle.AutoSize = true;
+            this.labelPageTitle.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPageTitle.Location = new System.Drawing.Point(48, 36);
+            this.labelPageTitle.Name = "labelPageTitle";
+            this.labelPageTitle.Size = new System.Drawing.Size(460, 47);
+            this.labelPageTitle.TabIndex = 0;
+            this.labelPageTitle.Text = "Text Diary Entry App";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(50, 122);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(120, 33);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Title: ";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(50, 174);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(105, 33);
+            this.labelDate.TabIndex = 2;
+            this.labelDate.Text = "Date: ";
+            // 
+            // labelEntry
+            // 
+            this.labelEntry.AutoSize = true;
+            this.labelEntry.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEntry.Location = new System.Drawing.Point(50, 228);
+            this.labelEntry.Name = "labelEntry";
+            this.labelEntry.Size = new System.Drawing.Size(120, 33);
+            this.labelEntry.TabIndex = 3;
+            this.labelEntry.Text = "Entry: ";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxTitle.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBoxTitle.Location = new System.Drawing.Point(175, 122);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(542, 31);
+            this.textBoxTitle.TabIndex = 4;
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxDate.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBoxDate.Location = new System.Drawing.Point(175, 174);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(542, 31);
+            this.textBoxDate.TabIndex = 5;
+            // 
+            // textBoxEntry
+            // 
+            this.textBoxEntry.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxEntry.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEntry.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.textBoxEntry.Location = new System.Drawing.Point(175, 228);
+            this.textBoxEntry.Multiline = true;
+            this.textBoxEntry.Name = "textBoxEntry";
+            this.textBoxEntry.Size = new System.Drawing.Size(542, 114);
+            this.textBoxEntry.TabIndex = 7;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSubmit.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSubmit.Location = new System.Drawing.Point(175, 361);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(542, 57);
+            this.buttonSubmit.TabIndex = 8;
+            this.buttonSubmit.Text = "Submit Entry";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBoxEntry);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.labelEntry);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.labelPageTitle);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelPageTitle;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelEntry;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.TextBox textBoxEntry;
+        private System.Windows.Forms.Button buttonSubmit;
+    }
+}
+
---
+++ Basic Text Diary App/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Basic_Text_Diary_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            string Title = textBoxTitle.Text;
+            string Date = textBoxDate.Text;
+            string Entry = textBoxEntry.Text;
+            string fileName = $"{Title}.txt";
+
+            File.Create(fileName).Close();
+            using (StreamWriter sw = File.AppendText(fileName))
+            {
+                sw.WriteLine("Entry Title: " + Title);
+                sw.WriteLine("\n");
+                sw.WriteLine("Entry Date: " + Date);
+                sw.WriteLine("\n");
+                sw.WriteLine(Entry);
+
+            }
+        }
+    }
+}
